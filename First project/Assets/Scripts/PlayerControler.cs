733698049547c44e322763ef02c791b7dca151d8
Hi <sadie.a948@stu.nebo.edu>
2022-08-22 09:03:31 -0600

    Added Unity First Project

---
+++ First project/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public int speed; //set speed
+    public float hInput; // horizontal input
+    public float vInput; // vertical input
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
