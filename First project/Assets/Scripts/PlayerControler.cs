b9745925e175a8870ed4e6ab11ea73e2340c416a
Hi <sadie.a948@stu.nebo.edu>
2022-08-26 09:21:07 -0600

    Adding waves and birds (White Boxing)

--- First project/Assets/Scripts/PlayerControler.cs
+++ First project/Assets/Scripts/PlayerControler.cs
@@ -24,7 +24,7 @@ public class PlayerControler : MonoBehaviour
         vInput = Input.GetAxis("Vertical");
 
         //move the character around
-        transform.Rotate(Vector3.up, rotSpeed * hInput * Time.deltaTime); //Left and Right Movement
+        transform.Rotate(Vector3.up, rotSpeed * hInput * Time.deltaTime); //Rotate Left and Right
 
         transform.Translate(Vector3.forward * speed * vInput * Time.deltaTime); //Forward and Backward Movement
     }
