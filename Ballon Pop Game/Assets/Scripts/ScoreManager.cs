3b0eefde366c168cb88635ee0a7e67ce6e95e6c9
Hi <sadie.a948@stu.nebo.edu>
2022-09-20 09:21:06 -0600

    More Coding.

--- Ballon Pop Game/Assets/Scripts/ScoreManager.cs
+++ Ballon Pop Game/Assets/Scripts/ScoreManager.cs
@@ -1,14 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
+    public int score; //keep track of the score
+    public TextMeshProUGUI scoreText; //Text UI object to be modified
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateScoreText(); //Call UpdateScoreText function
     }
+
+    public void IncreaseScoreText(int amount)
+    {
+        score += amount; //Increase score by amount
+        UpdateScoreText(); //Update score UI text
+    }
+
+    public void DecreaseScoreText(int amount)
+    {
+        score -= amount;
+        UpdateScoreText();
+    }
+
+    public void UpdateScoreText()
+    {
+        scoreText.text = "Sxcore: " + score;
+    }
+
 
     // Update is called once per frame
     void Update()
