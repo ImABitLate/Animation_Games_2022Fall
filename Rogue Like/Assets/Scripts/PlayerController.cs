c9b31a572b1070f0879e21a6150877061ebda55d
Hi <sadie.a948@stu.nebo.edu>
2022-09-27 09:19:00 -0600

    Rogue game.

---
+++ Rogue Like/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed; //how fast the player moves
+    private float hInput; //horizontal input
+    private float vInput; //vertical input
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
+        transform.translate(Vector2.right * hInput * speed * Time.deltaTime); //Move left and right
+
+        transform.translate(Vector2.up * vInput * speed * Time.deltaTime); //Move up or down
+
+    }
+}
