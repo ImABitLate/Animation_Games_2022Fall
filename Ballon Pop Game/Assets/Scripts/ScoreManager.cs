73df309c575510bd454157a061c1f19f36bc04f1
Hi <sadie.a948@stu.nebo.edu>
2022-09-27 08:27:43 -0600

    Trying to fix score script.

--- Ballon Pop Game/Assets/Scripts/ScoreManager.cs
+++ Ballon Pop Game/Assets/Scripts/ScoreManager.cs
@@ -30,11 +30,4 @@ public class ScoreManager : MonoBehaviour
     {
         scoreText.text = "Score: " + score;
     }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
